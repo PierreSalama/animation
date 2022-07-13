331093a0792b8373edb47db9624539cbbaf12f3e
PierreSalama <102556631+PierreSalama@users.noreply.github.com>
2022-07-13 08:01:17 -0400

    Add files via upload

---
+++ square.py
@@ -0,0 +1,16 @@
+from operator import le
+from turtle import *
+
+
+speed(0)
+bgcolor("black")
+pensize(1)
+color("white")
+for x in range(300):
+    clear()
+    for i in range(40):
+        forward(9)
+        left(9)
+
+    
+    
